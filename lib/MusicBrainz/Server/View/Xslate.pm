0d1367963a2299c8974e16e7074502c5684fa165
Oliver Charles <oliver.g.charles@googlemail.com>
2012-01-19 16:23:43 +0000

    Moving towards the current layout

--- lib/MusicBrainz/Server/View/Xslate.pm
+++ lib/MusicBrainz/Server/View/Xslate.pm
@@ -1,12 +1,18 @@
 package MusicBrainz::Server::View::Xslate;
 use Moose;
 
+use MusicBrainz::Server::Data::Utils qw( ref_to_type );
+use MusicBrainz::Server::Track;
+
 extends 'Catalyst::View::Xslate';
 
 has '+function' => (
     default => sub { +{
-        l => sub { shift }
-    }}
+        l => sub { shift },
+        ln => sub { shift },
+        format_duration => \&MusicBrainz::Server::Track::FormatTrackLength,
+        entity_type => \&ref_to_type
+    }},
 );
 
 1;
